c89c5bd4ad2c52a5c22c18b66fad672737ff829f
jim <jim@semfoundry.com>
2026-07-19 16:41:49 +0700

    fix: tighten game hud banner packing

--- apps/web/src/test/game-hud-layout.test.ts
+++ apps/web/src/test/game-hud-layout.test.ts
@@ -17,15 +17,18 @@ describe('game HUD layout', () => {
       { x: 384, y: 256, width: 64, height: 64 },
       { x: 512, y: 256, width: 172, height: 98 },
     ]);
+    expect(gameHudLayout.bannerPieces.map((piece) => piece.target.x)).toEqual([
+      -63, -16, 16, 59, -63, 0, 59, -63, -16, 16, 63,
+    ]);
   });
 
   it('removes atlas whitespace and centers the wood table on the packed banner', () => {
     expect(gameHudLayout.bannerBounds).toEqual({
-      left: -122,
+      left: -110,
       top: -56.5,
-      right: 122,
+      right: 106,
       bottom: 56.5,
-      width: 244,
+      width: 216,
       height: 113,
     });
     expect(gameHudLayout.woodTableSlotsOffset).toEqual({
